e382a7d1a4cd690f50fe08de6cbb17da16b4b463
Nikalya_1398 <v.ninmathirasa@gmail.com>
2023-10-19 15:31:45 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,8 @@
+
+// let a = document.getElementById("one")
+
+// let b = a.innerHTML("Subtitle")
+
+let heading = document.getElementById("title")
+heading.textContent="Bye World"
+console.log(heading.textContent)
